a63ed1d8552c56a451017152801221c785307c74
Scott <ScottAdelman97@gmail.com>
2022-07-07 12:32:35 -0600

    Play the guessing game!

---
+++ RandomNumberGame/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Enter a number between 1 and 100:");
+var r = new Random();
+var randomNumber = r.Next(1, 100);
+bool gameOver = false;
+
+do
+{
+    var userInput = int.Parse(Console.ReadLine());
+
+    if (userInput > randomNumber)
+    {
+        Console.WriteLine("Too High");
+    }
+
+    else if (userInput < randomNumber)
+    {
+        Console.WriteLine("Too Low");
+    }
+
+    else
+    {
+        Console.WriteLine("You got it! Good guess!");
+        gameOver = true;
+    }
+
+}
+
+while (gameOver == false);
